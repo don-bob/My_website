16e3b71f67f5e27f555d2edeee0b4a841e6ad487
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-11-16 19:28:32 -0500

    se quito assets/ignore

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from dash import Dash, html, page_container
 app = Dash(
     __name__, use_pages=True, 
     suppress_callback_exceptions=True,
-    assets_ignore=r'.*\.css'
 )
 
 server = app.server
